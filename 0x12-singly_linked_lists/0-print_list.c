18335b009815c1dff209ff8a603852107d1528c2
Sheyman13214 <oluwaseyiowolabi40@gmail.com>
2023-08-24 12:28:12 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,15 +12,15 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 
 	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+{
+	if (!h->str)
+	printf("[0] (nil)\n");
+	else
+	printf("[%u] %s\n", h->len, h->str);
 
-		h = h->next;
-		count++;
-	}
+	h = h->next;
+	count++;
+}
 
 	return (count);
 }
